78dcafb9e3aa4bff337b394e25448eb96288498f
marcelldac <devmarcell@protonmail.com>
2024-06-30 01:08:41 -0300

    Adicionar arquivos de projeto.

---
+++ ProductControl/Data/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductControl.Data
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options): base(options) { 
+            
+        }
+    }
+}
---
+++ ProductControl/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductControl.Models
+{
+    public class ProductModel
+    {
+        public Int64? Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string BarCode { get; set; } = string.Empty;
+        public DateTime ValidityDate { get; set; }
+        public DateTime RegistryDate { get; set; }
+        public int Quantity { get; set; }
+        public Decimal Price { get; set; }
+        public string PhotoName { get; set; }
+        public byte[] Photo { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
