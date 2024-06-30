b052161bbb327469893314b7c3e22b6ef635acb4
marcelldac <devmarcell@protonmail.com>
2024-06-30 01:44:00 -0300

    feat: create database context

--- ProductControl/Data/DatabaseContext.cs
+++ ProductControl/Data/DatabaseContext.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProductControl.Models;
 
 namespace ProductControl.Data
 {
     public class DatabaseContext: DbContext
     {
-        public DatabaseContext(DbContextOptions<DatabaseContext> options): base(options) { 
-            
+        public DatabaseContext(DbContextOptions<DatabaseContext> options): base(options)
+        {
         }
+
+        public DbSet<ProductModel> Product { get; set; }
     }
 }
