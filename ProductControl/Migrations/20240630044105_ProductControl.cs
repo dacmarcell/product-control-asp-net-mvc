6ced3f151ff2a7d5ab2841e50e5a62671ca6d7d7
marcelldac <devmarcell@protonmail.com>
2024-06-30 01:44:36 -0300

    chore: add migrations

---
+++ ProductControl/Migrations/20240630044105_ProductControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductControl.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductControl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BarCode = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    ValidityDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RegistryDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    PhotoName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    Active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_BarCode",
+                table: "Product",
+                column: "BarCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
